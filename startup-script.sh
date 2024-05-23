b6d15a624f175e8182923e3a2424d916090f210d
dr3jitsu <97427226+dr3jitsu@users.noreply.github.com>
2024-05-23 21:56:37 +0700

    Update startup-script.sh

--- startup-script.sh
+++ startup-script.sh
@@ -14,7 +14,7 @@ git clone https://github.com/dr3jitsu/KafkaQuiz.git .
 #sudo bash $HOME/flask-app/add-google-cloud-ops-agent-repo.sh --also-install
 
 # Account to own server process
-useradd -m -d $HOME/flask-ap flask-ap
+useradd -m -d $HOME/flask-app flask-app
 
 # Python environment setup
 virtualenv -p python3 $HOME/flask-app/env
@@ -22,7 +22,7 @@ virtualenv -p python3 $HOME/flask-app/env
 $HOME/flask-app/env/bin/pip install -r $HOME/flask-app/requirements.txt
 
 # Set ownership to newly created account
-chown -R flask-ap:flask-ap $HOME/flask-app
+chown -R flask-app:flask-app $HOME/flask-app
 
 # Put supervisor configuration in proper place
 #cp flask-ap/python-app.conf /etc/supervisor/conf.d/python-app.conf
