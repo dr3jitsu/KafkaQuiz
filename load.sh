779d1a27013d066cd75ae1af48d774f6b91eafcb
dr3jitsu <97427226+dr3jitsu@users.noreply.github.com>
2024-05-23 21:13:38 +0700

    Update load.sh

--- load.sh
+++ load.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-export CONFLUENT_HOME=/Users/ahartono/Downloads/workingspace/confluent-7.2.1
-export PATH=$PATH:$CONFLUENT_HOME/bin
-export PATH=/Users/ahartono/Downloads/workingspace/kafka-docker-playground/scripts/cli:$PATH
 virtualenv env
 source env/bin/activate
 echo "Run :     source env/bin/activate"
